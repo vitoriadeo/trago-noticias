4e352b494809f89051064b61d90ac5ac0512bce1
Vitória de Oliveira <vio1995>
2025-08-15 16:05:27 -0300

    add páginas públicas about e privacy

--- app/controllers/home_controller.py
+++ app/controllers/home_controller.py
@@ -5,3 +5,11 @@ bp = Blueprint('home', __name__)
 @bp.route('/')
 def index():
     return render_template('index.html')
+
+@bp.route('/about')
+def about():
+    return render_template('about.html')
+
+@bp.route('/privacy')
+def privacy():
+    return render_template('privacy.html')
