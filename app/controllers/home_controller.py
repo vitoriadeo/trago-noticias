bc5b3bbff5a402dc651964d2ad33841d84ef3126
Vitória de Oliveira <vitoria.oliveira513@gmail.com>
2025-08-29 13:22:15 -0300

    feat(wtforms): adicionar flask forms/flask-wtf ao formulário de termos
    adiciona recursos do flask wtforms ao html e ao home_controller para lidar de recepção dados

--- app/controllers/home_controller.py
+++ app/controllers/home_controller.py
@@ -1,4 +1,8 @@
 from flask import Blueprint, render_template, request
+from flask_wtf import FlaskForm
+from wtforms import StringField, SelectField, EmailField
+from wtforms.validators import DataRequired, Email, Length, AnyOf
+
 import requests
 
 from flask import current_app
@@ -6,50 +10,80 @@ from flask import current_app
 bp = Blueprint("home", __name__)
 
 
-@bp.route("/")
+class Form(FlaskForm):
+    name = StringField("Seu nome", validators=[DataRequired(), Length(min=2, max=100)])
+    word = StringField(
+        "Termo para monitorar", validators=[DataRequired(), Length(min=2, max=100)]
+    )
+    email = EmailField("Endereço de e-mail", validators=[DataRequired(), Email()])
+    frequency = SelectField(
+        "Frequência dos alertas",
+        choices=[
+            ("", "Selecione a frequência"),
+            ("diario", "Diariamente"),
+            ("semanal", "Semanalmente"),
+            ("mensal", "Mensalmente"),
+            ("anual", "Anualmente"),
+        ],
+        validators=[
+            DataRequired(message='Por favor, selecione uma frequência para o alerta.'),
+            AnyOf(
+                values=[
+                    ("diario"),
+                    ("semanal"),
+                    ("mensal"),
+                    ("anual"),
+                ],
+            message='Por favor, escolha uma opção válida.'),
+        ],
+    )
+
+
+@bp.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
+    form = Form()
 
+    if form.validate_on_submit():
+        try:
+            recaptcha_token = request.form.get("recaptcha-response")
 
-@bp.route("/cadastrar", methods=["POST"])
-def cadastrar():
-    try:
-        recaptcha_token = request.form.get("recaptcha-response")
+            secret_key = current_app.config["RECAPTCHA_SECRET_KEY"]
+            verification_url = "https://www.google.com/recaptcha/api/siteverify"
+            payload = {"secret": secret_key, "response": recaptcha_token}
 
-        secret_key = current_app.config["RECAPTCHA_SECRET_KEY"]
-        verification_url = "https://www.google.com/recaptcha/api/siteverify"
-        payload = {"secret": secret_key, "response": recaptcha_token}
+            response = requests.post(verification_url, data=payload)
+            result = response.json()
 
-        response = requests.post(verification_url, data=payload)
-        result = response.json()
+            print("Resposta do Google reCAPTCHA:", response, result)
 
-        print("Resposta do Google reCAPTCHA:", response, result)
+            name = form.name.data
+            word = form.word.data
+            email = form.email.data
+            frequency = form.frequency.data
 
+            # coisas do db e outros babados vão aqui.
 
-        name = request.form["name"]
-        termo = request.form["termo"]
-        email = request.form["email"]
-        frequencia = request.form["frequencia"]
+            if result["success"] and result["score"] >= 0.5:
+                # logica do db aqui
+                mensagem_sucesso = f"Alerta para '{word}' criado com sucesso!"
+                return render_template(
+                    "index.html", form=form, status="sucesso", mensagem=mensagem_sucesso
+                )
+            else:
+                mensagem_falha = "Falha na verificação reCAPTCHA. Tente novamente."
+                return render_template(
+                    "index.html", form=form, status="falha", mensagem=mensagem_falha
+                )
 
-        # coisas do db e outros babados vão aqui.
+        except Exception as erro:
+            print(f"Ocorreu um erro: {erro}")
 
-        if result["success"] and result["score"] >= 0.5:
-            # logica do db aqui
-            mensagem_sucesso = f"Alerta para '{termo}' criado com sucesso!"
+            mensagem_falha = "Vish! Ocorreu um erro interno."
             return render_template(
-                "index.html", status="sucesso", mensagem=mensagem_sucesso
+                "index.html", form=form, status="falha", mensagem=mensagem_falha
             )
-        else:
-            mensagem_falha = (
-                "Falha na verificação reCAPTCHA. Tente novamente."
-            )
-        return render_template("index.html", status="falha", mensagem=mensagem_falha)
 
-    except Exception as e:
-        print(f"Ocorreu um erro: {e}")
-
-        mensagem_falha = "Vish! Ocorreu um erro interno."
-        return render_template("index.html", status="falha", mensagem=mensagem_falha)
+    return render_template("index.html", form=form)
 
 
 @bp.route("/about")
