9734faf9bbc5731a5a05eb0194545704f4cd0dc2
Vitória de Oliveira <vitoria.oliveira513@gmail.com>
2025-08-29 13:15:03 -0300

    refactor(config): separar ambientes prod/dev
    separação dos ambientes de desenvolvimento e produção. adicão de configurações especiais para cada ambiente.

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,15 @@
+import os 
 from flask import Flask
-from .config import ProdConfig
-
-app = Flask(__name__)
+from .config import ProdConfig, DevConfig
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object(ProdConfig)
+    
+    if os.environ.get('FLASK_ENV') == 'production':
+        app.config.from_object(ProdConfig)
+
+    else:
+        app.config.from_object(DevConfig)
 
     from app.controllers import home_controller
     app.register_blueprint(home_controller.bp)
--- app/config.py
+++ app/config.py
@@ -1,8 +1,16 @@
 import os
 
 class Config:
-    SECRET_KEY = "minhachave"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+
 
 class ProdConfig(Config):
+    DEBUG = False
+    RECAPTCHA_SITE_KEY = os.environ.get('RECAPTCHA_SITE_KEY')
+    RECAPTCHA_SECRET_KEY = os.environ.get('RECAPTCHA_SECRET_KEY')
+
+class DevConfig(Config):
+    DEBUG = True
     RECAPTCHA_SITE_KEY = os.environ.get('RECAPTCHA_SITE_KEY')
     RECAPTCHA_SECRET_KEY = os.environ.get('RECAPTCHA_SECRET_KEY')
