183678799b31f5110649d5f49c4e2b6cff50e2d6
Vitória de Oliveira <vitoria.oliveira513@gmail.com>
2025-09-10 18:59:16 -0300

    refactor(form): criar form.py e mover formulário de termo
    move formulário de termo para form.py
    
    #6

---
+++ app/form.py
@@ -0,0 +1,31 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SelectField, EmailField
+from wtforms.validators import DataRequired, Email, Length, AnyOf
+
+class Form(FlaskForm):
+    name = StringField("Seu nome", validators=[DataRequired(), Length(min=2, max=100)])
+    word = StringField(
+        "Termo para monitorar", validators=[DataRequired(), Length(min=2, max=100)]
+    )
+    email = EmailField("Endereço de e-mail", validators=[DataRequired(), Email()])
+    frequency = SelectField(
+        "Frequência dos alertas",
+        choices=[
+            ("", "Selecione a frequência"),
+            ("diario", "Diariamente"),
+            ("semanal", "Semanalmente"),
+            ("mensal", "Mensalmente"),
+            ("anual", "Anualmente"),
+        ],
+        validators=[
+            DataRequired(message='Por favor, selecione uma frequência para o alerta.'),
+            AnyOf(
+                values=[
+                    ("diario"),
+                    ("semanal"),
+                    ("mensal"),
+                    ("anual"),
+                ],
+            message='Por favor, escolha uma opção válida.'),
+        ],
+    )
