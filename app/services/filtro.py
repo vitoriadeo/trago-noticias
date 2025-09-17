8f783370f003ddcc66ec41bb6d6aa6fd622997e4
Vitória de Oliveira <vitoria.oliveira513@gmail.com>
2025-09-17 18:14:14 -0300

    refactor(path): adicionar caminho relativo para allowlis.txt
    add caminho mais dinamico para acessar a allowlist.txt

--- app/services/filtro.py
+++ app/services/filtro.py
@@ -1,16 +1,18 @@
 from web_scraper import coleta
+from pathlib import Path
 
 resultado = coleta(termo)
 
-caminho_arquivo = r'C:/Users/Vitória/Documents/projetos/trago-noticias/app/'
-caminho_arquivo += 'allowlist.txt'
+caminho_atual = Path(__file__).resolve()
+caminho_arquivo = caminho_atual.parent.parent
+caminho_arquivo = caminho_arquivo / 'allowlist.txt'
+
+caminho_arquivo = Path('app/allowlist.txt')
 
 with open(caminho_arquivo, 'r', encoding='utf-8') as arquivo:
     dominios = [sites.split(', ') for sites in arquivo]
 
 dominios = set(dominios[0])
-
-#fonte = resultado[0]['Fonte']
 
 noticias_filtradas = []
 
