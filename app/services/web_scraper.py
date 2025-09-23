4fa5659146bc64da71f44463a9c3a82faf4cc576
Vitória de Oliveira <vitoria.oliveira513@gmail.com>
2025-09-23 19:17:00 -0300

    refactor(try/except): adicionar tratamento de erro try/except
    add um tratamento de erro básico para problemas que podem surgir
    
    close #11

--- app/services/web_scraper.py
+++ app/services/web_scraper.py
@@ -4,38 +4,43 @@ from urllib.parse import urljoin
 
 # OBS: ADD LÓGICA PARA QUANDO DIV CLAS MUDAR. ALERTA VIA EMAIL.
 
-dicionario_de_noticias = {}
-resultados_noticias = []
-base_url = "https://news.google.com"
-
 def coleta(termo):
+    dicionario_de_noticias = {}
+    resultados_noticias = []
+    base_url = "https://news.google.com"
+
     print(f"Iniciando buscas por {termo}...")
 
-    resposta = requests.get(f'https://news.google.com/search?q={termo}&hl=pt-BR&gl=BR&ceid=BR%3Apt-419')
-    
-    sopa = BeautifulSoup(resposta.text, "html.parser")
-    amontoado_html = sopa.find_all("div", class_="m5k28") #div class m5k28 em funcionamento data 14/09/2025
+    try:
+        resposta = requests.get(f'https://news.google.com/search?q={termo}&hl=pt-BR&gl=BR&ceid=BR%3Apt-419')
+        resposta.raise_for_status()
 
-    for noticia in amontoado_html:
-        tag_a = noticia.find('a')
+        sopa = BeautifulSoup(resposta.text, "html.parser")
+        amontoado_html = sopa.find_all("div", class_="m5k28", limit=5) #div class m5k28 em funcionamento data 14/09/2025
 
-        if tag_a:
-            link_relativo = tag_a['href']
-        else:
-            print('SEM LINK. PULANDO!!')
-            continue
+        for noticia in amontoado_html:
+            tag_a = noticia.find('a')
 
-        link_completo = urljoin(base_url, link_relativo)
+            if tag_a:
+                link_relativo = tag_a['href']
+            else:
+                print('SEM LINK. PULANDO!!')
+                continue
 
-        tituloBruto = noticia.get_text()
-        fonte_titulo = tituloBruto.split('Mais', 1)
-        titulo = fonte_titulo[1]
-        fonte = fonte_titulo[0]
+            link_completo = urljoin(base_url, link_relativo)
 
-        dicionario_de_noticias['Fonte'] = fonte
-        dicionario_de_noticias['Titulo'] = titulo
-        dicionario_de_noticias['Link completo'] = link_completo
+            tituloBruto = noticia.get_text()
+            fonte_titulo = tituloBruto.split('Mais', 1)
+            titulo = fonte_titulo[1]
+            fonte = fonte_titulo[0]
 
-        resultados_noticias.append(dicionario_de_noticias.copy())
+            dicionario_de_noticias['Fonte'] = fonte
+            dicionario_de_noticias['Titulo'] = titulo
+            dicionario_de_noticias['Link completo'] = link_completo
 
-    return resultados_noticias
+            resultados_noticias.append(dicionario_de_noticias.copy())
+
+        return resultados_noticias
+    except requests.exceptions.RequestException as e:
+        print(f"LOG - Erro de conexão: {e}")
+        return None
