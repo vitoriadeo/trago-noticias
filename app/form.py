5bc305f1862762d858372a3263a633679169ba5f
Vitória de Oliveira <vitoria.oliveira513@gmail.com>
2025-09-23 20:52:57 -0300

    feat(form): alterar opções de frequencia
    alterar opções de frequencia no form
    
    close #15

--- app/form.py
+++ app/form.py
@@ -2,23 +2,23 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SelectField, EmailField
 from wtforms.validators import DataRequired, Email, Length, AnyOf
 
+
 class Form(FlaskForm):
     name = StringField("Seu nome", validators=[DataRequired(), Length(min=2, max=100)])
     word = StringField(
-        "Termo para monitorar", validators=[DataRequired(), Length(min=2, max=100)]
+        "Termo para monitorar",
+        validators=[DataRequired(), Length(min=2, max=100)],
     )
     email = EmailField("Endereço de e-mail", validators=[DataRequired(), Email()])
     frequency = SelectField(
         "Frequência dos alertas",
         choices=[
             ("", "Selecione a frequência"),
-            ("diario", "Diariamente"),
-            ("semanal", "Semanalmente"),
-            ("mensal", "Mensalmente"),
-            ("anual", "Anualmente"),
+            ("once", "Somente uma vez, quando uma novidade aparecer"),
+            ("realtime", "Sempre que uma novidade aparecer"),
         ],
         validators=[
-            DataRequired(message='Por favor, selecione uma frequência para o alerta.'),
+            DataRequired(message="Por favor, selecione uma frequência para o alerta."),
             AnyOf(
                 values=[
                     ("diario"),
@@ -26,6 +26,7 @@ class Form(FlaskForm):
                     ("mensal"),
                     ("anual"),
                 ],
-            message='Por favor, escolha uma opção válida.'),
+                message="Por favor, escolha uma opção válida.",
+            ),
         ],
     )
