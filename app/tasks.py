bd7753baaba053188de0ae764981c6293237013b
Vitória de Oliveira <vitoria.oliveira513@gmail.com>
2025-10-16 20:27:18 -0300

    fix(cronjob): correção de contexto

--- app/tasks.py
+++ app/tasks.py
@@ -1,11 +1,12 @@
 import click
 from .services.alert_service import process_alerts
-
+from . import app
 
 @click.command('initTasks')
 def task_maestro(): #Busca, execução, filtro e envio
     click.echo('Rotina de busca iniciada...')
 
-    process_alerts()
+    with app.app_context():
+        process_alerts()
 
     click.echo('Rotina finalizada!')
