f0442f23759863f1869fdb7c0d951bd7125fa4ca
Vitória de Oliveira <vitoria.oliveira513@gmail.com>
2025-10-28 20:23:35 -0300

    fix(tasks.py): tratar exceção no task_maestro para evitar crash
    BREAKING CHANGE: ref #20

--- app/tasks.py
+++ app/tasks.py
@@ -4,11 +4,15 @@ from app import create_app
 
 
 @click.command('initTasks')
-def task_maestro(): #Busca, execução, filtro e envio
-    click.echo('Rotina de busca iniciada...')
-    
-    app = create_app()
-    with app.app_context():
-        process_alerts()
+def task_maestro():
+    try:
+        click.echo('Rotina de busca iniciada...')
+        
+        app = create_app()
+        with app.app_context():
+            process_alerts()
 
-    click.echo('Rotina finalizada!')
+        click.echo(click.style('Rotina finalizada com sucesso!', fg='green'))
+
+    except Exception as e:
+        click.echo(click.style("ERRO: Rotina interrompida. >> Verifique o e-mail de erro para detalhes.", fg='red'))
