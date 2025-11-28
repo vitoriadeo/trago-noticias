6d7b284b1fa887d3b0e82b857a3894544982f271
Vitória de Oliveira <vitoria.oliveira513@gmail.com>
2025-11-28 20:09:46 -0300

    fix(logging): retirar logging de debug

--- run.py
+++ run.py
@@ -9,11 +9,15 @@ from app import create_app
 import logging
 
 logging.basicConfig(
-    level=logging.DEBUG,
+    level=logging.INFO,
     encoding='utf-8',
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     handlers=[logging.FileHandler('run.log'), logging.StreamHandler()]
 )
+
+logging.getLogger("urllib3").setLevel(logging.WARNING)
+logging.getLogger("python_http_client").setLevel(logging.WARNING)
+logging.getLogger("werkzeug").setLevel(logging.WARNING)
 
 app = create_app()
 
